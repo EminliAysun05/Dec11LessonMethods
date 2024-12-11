33125a174768e828863646abcae88557f8ce79a3
EminliAysun05 <aeminli232@gmail.com>
2024-12-11 22:51:18 +0400

    add new code line

--- 11Dec/Program.cs
+++ 11Dec/Program.cs
@@ -2,108 +2,110 @@
 {
     internal class Program
     {
-        //static void Main(string[] args)
-        //{
-        //    //methodlar
 
-        //    //sade
-        //    Console.WriteLine("Salam, Elvin!");
-        //    Console.WriteLine("Salam, Aysel!");
-        //    Console.WriteLine("Salam, Orxan!");
+        static void Main(string[] args)
+        {
+            //methodlar
 
+            //sade
+            Console.WriteLine("Salam, Elvin!");
+            Console.WriteLine("Salam, Aysel!");
+            Console.WriteLine("Salam, Orxan!");
 
+            Console.WriteLine("for github testing");
 
-        //}
-        //with methods
-        //static void Mains(string[] args)
-        //{
-        //    SayHello("Elvin");
-        //    SayHello("Aysel");
-        //    SayHello("Orxan");
-        //}
+            //}
+            //with methods
+            //static void Mains(string[] args)
+            //{
+            //    SayHello("Elvin");
+            //    SayHello("Aysel");
+            //    SayHello("Orxan");
+            //}
 
-        //static void SayHello(string name)
-        //{
-        //    Console.WriteLine("Salam, " + name + "!");
-        //}
+            //static void SayHello(string name)
+            //{
+            //    Console.WriteLine("Salam, " + name + "!");
+            //}
 
-        //numune 2 
-        //static void Main(string[] args)
-        //{
-        //    int a = 5;
-        //    int b = 10;
-        //    int sum = a + b;
-        //    Console.WriteLine("Cəm: " + sum);
-        //}
+            //numune 2 
+            //static void Main(string[] args)
+            //{
+            //    int a = 5;
+            //    int b = 10;
+            //    int sum = a + b;
+            //    Console.WriteLine("Cəm: " + sum);
+            //}
 
-        //numune 2 with methods
+            //numune 2 with methods
 
-        //static void Main(string[] args)
-        //{
-        //    int result = Add(15, 25);
-        //    Console.WriteLine("Hasil: " + result);
-        //}
+            //static void Main(string[] args)
+            //{
+            //    int result = Add(15, 25);
+            //    Console.WriteLine("Hasil: " + result);
+            //}
 
-        //static int Add(int x, int y)
-        //{
-        //    return x - y ;
-        //}
+            //static int Add(int x, int y)
+            //{
+            //    return x - y ;
+            //}
 
-        //numune 3
+            //numune 3
 
-        //static void Main(string[] args)
-        //{
-        //    int grade1 = (90 + 85 + 80) / 3;
-        //    int grade2 = (70 + 75 + 80) / 3;
-        //    Console.WriteLine("Birinci şagirdin orta balı: " + grade1);
-        //    Console.WriteLine("İkinci şagirdin orta balı: " + grade2);
-        //}
+            //static void Main(string[] args)
+            //{
+            //    int grade1 = (90 + 85 + 80) / 3;
+            //    int grade2 = (70 + 75 + 80) / 3;
+            //    Console.WriteLine("Birinci şagirdin orta balı: " + grade1);
+            //    Console.WriteLine("İkinci şagirdin orta balı: " + grade2);
+            //}
 
-        //with methods
+            //with methods
 
-        //static void Main(string[] args)
-        //{
-        //    Console.WriteLine("Birinci şagirdin orta balı: " + CalculateAverage(90, 85, 80));
-        //    Console.WriteLine("İkinci şagirdin orta balı: " + CalculateAverage(70, 75, 80));
-        //}
+            //static void Main(string[] args)
+            //{
+            //    Console.WriteLine("Birinci şagirdin orta balı: " + CalculateAverage(90, 85, 80));
+            //    Console.WriteLine("İkinci şagirdin orta balı: " + CalculateAverage(70, 75, 80));
+            //}
 
-        //static int CalculateAverage(int x, int y, int z)
-        //{
-        //    return (x + y + z) / 5;
-        //}
+            //static int CalculateAverage(int x, int y, int z)
+            //{
+            //    return (x + y + z) / 5;
+            //}
 
-        //atm proqrami
+            //atm proqrami
 
-        //static void Main(string[] args)
-        //{
-            
-        //    WithdrawMoney();
-        //}
+            //static void Main(string[] args)
+            //{
 
-        //static void ShowMenu()
-        //{
-        //    Console.WriteLine("1. Balans yoxlamaq");
-        //    Console.WriteLine("2. Pul çıxarmaq");
-        //    Console.WriteLine("3. Pul yatırmaq");
-        //}
+            //    WithdrawMoney();
+            //}
 
-        //static void CheckBalance()
-        //{
-        //    Console.WriteLine("Sizin balansınız: 100 AZN");
-        //}
+            //static void ShowMenu()
+            //{
+            //    Console.WriteLine("1. Balans yoxlamaq");
+            //    Console.WriteLine("2. Pul çıxarmaq");
+            //    Console.WriteLine("3. Pul yatırmaq");
+            //}
 
-        //static void WithdrawMoney()
-        //{
-        //    Console.WriteLine("Pul çıxarıldı.");
-        //}
+            //static void CheckBalance()
+            //{
+            //    Console.WriteLine("Sizin balansınız: 100 AZN");
+            //}
 
-        //static void DepositMoney()
-        //{
-        //    Console.WriteLine("Pul yatırıldı.");
-       // }
-        //}
+            //static void WithdrawMoney()
+            //{
+            //    Console.WriteLine("Pul çıxarıldı.");
+            //}
+
+            //static void DepositMoney()
+            //{
+            //    Console.WriteLine("Pul yatırıldı.");
+            // }
+            //}
+        }
+
     }
-
 }
 
 
